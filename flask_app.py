d4d9780028da05ecd6f00d312b6461807c3560eb
Piyush <40433598+knightowl2704@users.noreply.github.com>
2019-08-20 06:50:19 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -7,11 +7,16 @@ import  pymysql.connections
 
 app = Flask(__name__)
 app.secret_key = 'developmentkey'
-app.config["RECAPTCHA_PUBLIC_KEY"] = '#######################################'
-app.config["RECAPTCHA_PRIVATE_KEY"] = '#######################################'
+app.config["RECAPTCHA_PUBLIC_KEY"] = 'YOUR_RECAPTCHA_PUBLIC_KEY_HERE(v2)'
+app.config["RECAPTCHA_PRIVATE_KEY"] = 'YOUR_RECAPTCHA_PRIVATE_KEY_HERE(v2)'
 app.config["TESTING"] = True
 
-mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'YOUR_DATABASE_USERNAME'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'YOUR_DATABASE_PASSWORD_HERE'
+app.config['MYSQL_DATABASE_DB'] = 'flaskappdata' #DATABASE_NAME
+
+mysql = MySQL(app)
 
 
 
@@ -30,7 +35,18 @@ class LoginForm(FlaskForm):
 def form():
    form = LoginForm()
    if form.validate():
-       return "Form is submitted"
+
+       if request.method == 'POST':
+           details = request.form
+           username = details['username']
+           password1 = details['password']
+           z = mysql.connect()
+           cur = z.cursor()
+           cur.execute("INSERT INTO flaskappdata(username,password1) VALUES (%s,MD5(%s))" , (username, password1))
+           z.commit()
+           cur.close()
+           return 'success'
+
    return render_template('index.html', form = form)
 
 if __name__ == '__main__':
